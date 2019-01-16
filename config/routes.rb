e3e4c22a9ed877a27550d71c5dd6eb97e5864383
Алдияр Амренов <aldiyarforattractor@gmail.com>
2019-01-16 19:03:58 +0600

    Реализован вход для студента

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root 'welcome#greetings'
-  
+  get 'students/:id/show' => 'students#show', as: 'student_show'
   devise_for :students
   devise_for :admins
   devise_for :users

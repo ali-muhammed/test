f898dfa8cc1917c85601a0cefec294b005a7e960
Aibek Izhanov <ibekable@gmail.com>
2019-01-22 20:34:22 +0600

    Слияние верстки с кодом

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,22 @@ Rails.application.routes.draw do
   devise_for :administrators
   devise_for :teachers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'greetings#index'
+  root 'welcome#greetings'
 
     namespace :administrator_cabinet do
         resources :welcome, only: [:index]
         resources :students
+        resources :teachers
     end
 
     namespace :student_cabinet do
         resources :welcome, only: [:index]
         resources :students, only: [:show]
+    end
+
+    namespace :teacher_cabinet do
+        resources :welcome, only: [:index]
+        resources :teachers, only: [:show]
     end
 
 
